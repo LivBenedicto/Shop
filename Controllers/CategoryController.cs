e828c0bcb96def6a07fa7a4cb24370e9c400abf3
LivBenedicto <livia.benedicto@hotmail.com>
2020-05-20 12:32:05 -0300

    Adding version and compression

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -9,7 +9,7 @@ using Shop.Models;
 
 namespace Shop.Controllers
 {
-    [Route("categories")]
+    [Route("version1/categories")]
     public class CategoryController : ControllerBase
     {
         [HttpGet]
